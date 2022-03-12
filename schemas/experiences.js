5b3ee787205dd9f928ad10783a4b8cf3a31c578f
motiur.rahman <motiur.rahman@technovista.com.bd>
2022-03-13 05:40:53 +0600

    sanity back-end

--- schemas/experiences.js
+++ schemas/experiences.js
@@ -1,18 +1,27 @@
-export default{
-    name:'experiences',
-    title:'Experiences',
+export default {
+    name: 'experiences',
+    title: 'Experiences',
     type: 'document',
-    fields:[
+    fields: [
         {
-            name:'year',
-            title:'Year',
-            type:'string'
+            name: 'year',
+            title: 'Year',
+            type: 'string'
         },
         {
-            name:'works',
-            title:'Works',
-            type:'array',
-            of:[{ type:'workExperience'}]
+            name: 'works',
+            title: 'Works',
+            type: 'array',
+            of: [{ type: 'workExperience' }]
         },
+    ],
+    orderings: [
+        {
+            title: 'Year',
+            name: 'yearDesc',
+            by: [
+                { field: 'year', direction: 'asc' }
+            ]
+        }
     ]
 }
